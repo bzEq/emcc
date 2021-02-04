c24bee8755d5ed9899aa627fd6e743c56f24bf9b
Kai Luo <gluokai@gmail.com>
2021-02-04 22:54:38 +0800

    DoAppend

--- tools/monkey_typing.cc
+++ tools/monkey_typing.cc
@@ -11,33 +11,37 @@ int main() {
   const size_t kMaxLines = 1 << 20;
   const size_t kMaxColumns = 1 << 16;
   auto DoInsert = [&]() {
-    size_t line = rnd.Next() * kMaxLines;
+    size_t line = std::min(buffer.CountLines(), size_t(rnd.Next() * kMaxLines));
     size_t column = rnd.Next() * kMaxColumns;
     size_t len =
         std::min(kMaxColumns - column, size_t(rnd.Next() * kMaxColumns));
-    printf("Inserting to %zu:%zu:%zu", line, column, len);
-    std::cout << std::endl;    
     for (size_t i = 0; i < len; ++i) {
       buffer.Insert(line, column, '0');
     }
+  };
+  auto DoAppend = [&]() {
+    if (rnd.Next() < 0.01)
+      buffer.Append(LineBuffer::kNewLine);
+    else
+      buffer.Append('0');
   };
   auto DoErase = [&]() {
     size_t line = rnd.Next() * kMaxLines;
     size_t column = rnd.Next() * kMaxColumns;
     size_t len =
         std::min(kMaxColumns - column, size_t(rnd.Next() * kMaxColumns));
-    printf("Erasing %zu:%zu:%zu", line, column, len);
-    std::cout << std::endl;    
     buffer.Erase(line, column, len);
   };
   size_t it = 0;
   while (true) {
-    printf("Iter #%zu: lines: %zu", it++, buffer.CountLines());
-    std::cout << std::endl;
-    if (rnd.Next() < 0.5)
+    printf("Iter #%zu: lines: %zu\n", it++, buffer.CountLines());
+    auto dice = rnd.Next();
+    if (dice < 0.1)
       DoErase();
-    else
+    else if (dice < 0.3)
       DoInsert();
+    else
+      DoAppend();
     assert(buffer.Verify());
   }
   return 0;
