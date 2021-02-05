9d506f89e44df6c9390eeb35f29bfa7ed53f610e
Kai Luo <gluokai@gmail.com>
2021-02-05 22:03:32 +0800

    Add MMapFile

--- support/sys.h
+++ support/sys.h
@@ -10,6 +10,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <assert.h>
 #include <functional>
 #include <iostream>
 #include <memory>
@@ -57,8 +58,8 @@ public:
     return membuf;
   }
 
-  static std::unique_ptr<MemoryBuffer> Truncate(const std::string &filename,
-                                                size_t length) {
+  static std::unique_ptr<MemoryBuffer> Create(const std::string &filename,
+                                              size_t length) {
     auto membuf =
         std::unique_ptr<MemoryBuffer>(new MemoryBuffer(filename, length));
     if (!membuf->buffer())
@@ -109,13 +110,114 @@ private:
   size_t length_;
 };
 
+class MMapFile {
+public:
+  static constexpr size_t kDefaultBlockSize = 8UL << 20;
+
+  struct Block {
+    char *data;
+    size_t length;
+    Block() : data(nullptr), length(0) {}
+
+    bool operator==(Block &other) {
+      return data == other.data && length == other.length;
+    }
+
+    bool operator!=(Block &other) { return !(*this == other); }
+
+    void reset() {
+      data = nullptr;
+      length = 0;
+    }
+  };
+
+  class iterator {
+  public:
+    iterator &operator++() {
+      assert(block_.data);
+      ::munmap(block_.data, block_.length);
+      offset_ += block_.length;
+      return Advance();
+    }
+
+    Block &operator*() { return block_; }
+
+    Block *operator->() { return &block_; }
+
+    bool operator==(iterator &other) { return block_ == other.block_; }
+
+    bool operator!=(iterator &other) { return block_ != other.block_; }
+
+  private:
+    friend class MMapFile;
+    iterator(int fd, size_t total_length, Block &block)
+        : fd_(fd), total_length_(total_length), offset_(0), block_(block) {}
+
+    iterator &Advance() {
+      if (offset_ >= total_length_) {
+        block_.reset();
+      } else {
+        if (offset_ + block_.length > total_length_)
+          block_.length = total_length_ - offset_;
+        block_.data =
+            (char *)::mmap(nullptr, block_.length, PROT_READ | PROT_WRITE,
+                           MAP_PRIVATE, fd_, offset_);
+        if (block_.data == MAP_FAILED)
+          block_.reset();
+      }
+      return *this;
+    }
+
+    int fd_;
+    const size_t total_length_;
+    off_t offset_;
+    Block &block_;
+  };
+
+  MMapFile(const std::string &filename) : fd_(-1), length_(0) {
+    fd_ = ::open(filename.c_str(), 0644, O_RDWR);
+    if (fd_ < 0)
+      return;
+    struct stat statbuf;
+    int err = ::fstat(fd_, &statbuf);
+    if (err < 0)
+      return;
+    length_ = statbuf.st_size;
+  }
+
+  iterator begin() {
+    size_t pgsize = sysconf(_SC_PAGE_SIZE);
+    block_.length = kDefaultBlockSize / pgsize * pgsize;
+    iterator it(fd_, length_, block_);
+    return it.Advance();
+  }
+
+  iterator end() { return iterator(fd_, length_, end_block_); }
+
+  bool IsOpen() const { return fd_ >= 0; }
+
+  size_t length() const { return length_; }
+
+  ~MMapFile() {
+    if (block_.data)
+      munmap(block_.data, block_.length);
+    if (fd_ >= 0)
+      ::close(fd_);
+  }
+
+private:
+  int fd_;
+  size_t length_;
+  Block block_, end_block_;
+};
+
 template <typename... Args>
-inline void Report(const char *prefix, Args &&... args) {
+inline void Report(const char *prefix, Args &&...args) {
   std::cerr << fmt::format("{}{}", prefix, std::forward<Args>(args)...) << "\n";
 }
 
 template <typename... Args>
-inline void Die(Args &&... args) {
+inline void Die(Args &&...args) {
   Report("fatal: ", std::forward<Args>(args)...);
   exit(1);
 }
---
+++ tools/echo_file.cc
@@ -0,0 +1,19 @@
+// Copyright (c) 2021 Kai Luo <gluokai@gmail.com>. All rights reserved.
+
+#include "editor/line_buffer.h"
+#include "support/sys.h"
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2)
+    emcc::Die("Usage %s: <filename>", argv[0]);
+  emcc::MMapFile file(argv[1]);
+  if (!file.IsOpen())
+    emcc::Die("Can't open %s", argv[1]);
+  emcc::LineBuffer buffer;
+  for (auto block : file)
+    std::cout << std::string(block.data, block.length);
+  return 0;
+}
