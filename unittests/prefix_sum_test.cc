5b9e5b484bb40f858834cdb523128e5531bf6431
Kai Luo <gluokai@gmail.com>
2021-02-09 02:56:23 +0800

    Test Add

--- unittests/prefix_sum_test.cc
+++ unittests/prefix_sum_test.cc
@@ -25,6 +25,20 @@ TEST(PrefixSumTest, OneToN) {
   EXPECT_TRUE(s.GetPrefixSum(100) == 5050);
 }
 
+TEST(PrefixSumTest, OneToN1) {
+  PrefixSum<int> s;
+  for (int i = 0; i <= 100; ++i)
+    s.Insert(i, i);
+  EXPECT_TRUE(s.size() == 101);
+  for (int i = 0; i <= 100; ++i)
+    EXPECT_TRUE(s.At(i) == i);
+  EXPECT_TRUE(s.size() == 101);
+  EXPECT_TRUE(s.GetPrefixSum(100) == 5050);
+  for (int i = 0; i <= 100; ++i)
+    s.Add(i, i);
+  EXPECT_TRUE(s.GetPrefixSum(100) == 2 * 5050);
+}
+
 TEST(PrefixSumTest, EraseTest) {
   PrefixSum<int> s;
   for (int i = 0; i <= 100; ++i)
