7eb4e38fa37660bb664325abf6736c6295d4b4f1
Kai Luo <gluokai@gmail.com>
2021-02-11 11:40:47 +0800

    Tune piece size

--- unittests/rope_benchmark.cc
+++ unittests/rope_benchmark.cc
@@ -11,7 +11,7 @@
 
 namespace {
 
-using Rope = emcc::Rope<char, std::basic_string, 1UL << 13>;
+using Rope = emcc::Rope<char, std::basic_string, 1UL << 12>;
 
 TEST(RopeBenchmark, ExtRopeRandomInsert) {
   emcc::Random rnd(std::time(nullptr));
