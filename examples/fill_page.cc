b4d717c51a076d22e3fdeb1e2eb9de82882cd1ee
Kai Luo <gluokai@gmail.com>
2021-02-12 22:17:44 +0800

    Measure time

---
+++ examples/fill_page.cc
@@ -0,0 +1,30 @@
+#include "support/misc.h"
+#include "support/sys.h"
+#include "tui/renderer.h"
+#include "tui/view.h"
+
+#include <chrono>
+
+int main(int argc, char *argv[]) {
+  using namespace emcc;
+  using namespace emcc::tui;
+  if (argc != 2)
+    Die("Usage: %s <filename>", argv[0]);
+  std::string filename(argv[1]);
+  auto buffer = LineBuffer::CreateFromFile(filename);
+  if (!buffer)
+    Die("Failed to open {}", filename);
+  int height = 40, width = 120;
+  Framebuffer framebuffer(width, height);
+  Page page(buffer.get(), &framebuffer, width, height);
+  auto start = std::chrono::high_resolution_clock::now();
+  page.Reload(0);
+  page.FillFrame(Cursor(0, 0), page.GetBoundary());
+  auto end = std::chrono::high_resolution_clock::now();
+  std::cout << "Elapsed time in microseconds : "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end -
+                                                                     start)
+                   .count()
+            << " µs" << std::endl;
+  return 0;
+}
