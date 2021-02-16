8586d9e005e30484086a426a94bc2aba29ea8e3e
Kai Luo <gluokai@gmail.com>
2021-02-16 21:47:33 +0800

    Refactor

--- tui/page.h
+++ tui/page.h
@@ -4,127 +4,13 @@
 #include "support/dynamic_array.h"
 #include "support/prefix_sum.h"
 #include "tui/cursor.h"
+#include "tui/framebuffer.h"
+#include "tui/pixel.h"
 
 #include <assert.h>
 #include <iostream>
 
 namespace emcc::tui {
-
-// Graphical representation of a charater, also offers map to character's
-// logical position.
-struct Pixel {
-  struct {
-    int character;
-  } shade;
-
-  struct {
-    uint8_t head_tail_pair; // A character might span multiple points.
-    size_t point;           // Offset at text buffer.
-  } position;
-
-  Pixel() { Reset(); }
-
-  void Reset() {
-    shade.character = ' ';
-    memset(&position, -1, sizeof(position));
-  }
-
-  bool operator==(const Pixel &other) {
-    return memcmp(this, &other, sizeof(*this)) == 0;
-  }
-
-  Pixel &operator=(const Pixel &other) {
-    memcpy(this, &other, sizeof(*this));
-    return *this;
-  }
-
-  bool is_same_position(const Pixel &other) const {
-    return memcmp(&this->position, &other.position, sizeof(this->position)) ==
-           0;
-  }
-
-  bool is_same_shade(const Pixel &other) const {
-    return memcmp(&this->shade, &other.shade, sizeof(this->shade)) == 0;
-  }
-
-  bool is_head() const {
-    return position.head_tail_pair >> (sizeof(position.head_tail_pair) * 8 - 1);
-  }
-  size_t offset() const {
-    return position.head_tail_pair &
-           (static_cast<decltype(position.head_tail_pair)>(~0U) >> 1);
-  }
-  size_t length() const {
-    assert(is_head());
-    return offset();
-  }
-  static Pixel MakeHeadPixel(size_t point, size_t len) {
-    Pixel p;
-    assert(len < (1 << (sizeof(p.position.head_tail_pair) * 8 - 1)));
-    p.position.head_tail_pair =
-        (1 << (sizeof(p.position.head_tail_pair) * 8 - 1)) | len;
-    p.position.point = point;
-    return p;
-  }
-  static Pixel MakeTailPixel(size_t point, size_t offset) {
-    Pixel p;
-    assert(offset > 0 &&
-           offset < (1 << (sizeof(p.position.head_tail_pair) * 8 - 1)));
-    p.position.head_tail_pair = offset;
-    p.position.point = point;
-    return p;
-  }
-  static std::vector<Pixel> MakeSeries(size_t offset, size_t len) {
-    assert(len > 0);
-    std::vector<Pixel> ret;
-    ret.emplace_back(MakeHeadPixel(offset, len));
-    for (size_t i = 1; i < len; ++i)
-      ret.emplace_back(MakeTailPixel(offset, i));
-    return ret;
-  }
-};
-
-// Graphical representation of a page.
-class Framebuffer {
-public:
-  friend class Page;
-  Framebuffer(size_t width, size_t height) : width_(width), height_(height) {
-    Reset();
-  }
-
-  void Resize(size_t width, size_t height) {
-    width_ = width;
-    height_ = height;
-  }
-
-  size_t width() const { return width_; }
-
-  size_t height() const { return height_; }
-
-  void Reset() {
-    frame_.clear();
-    frame_.resize(height_);
-    for (size_t i = 0; i < frame_.size(); ++i)
-      frame_[i].resize(width_);
-  }
-
-  const Pixel &Get(size_t y, size_t x) const { return frame_[y][x]; }
-
-  Pixel &Get(size_t y, size_t x) { return frame_[y][x]; }
-
-  template <typename... Args>
-  Framebuffer &Set(size_t y, size_t x, Args &&...args) {
-    auto &line = frame_[y];
-    line.emplace(line.begin() + x, std::forward<Args>(args)...);
-    return *this;
-  }
-
-  Cursor GetBoundary() const { return Cursor(height(), width()); }
-
-private:
-  size_t width_, height_;
-  std::vector<std::vector<Pixel>> frame_;
-};
 
 class Page {
 public:
---
+++ tui/pixel.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <vector>
+
+namespace emcc::tui {
+
+// Graphical representation of a charater, also offers map to character's
+// logical position.
+struct Pixel {
+  struct {
+    int character;
+  } shade;
+
+  struct {
+    uint8_t head_tail_pair; // A character might span multiple pixels.
+    size_t point; // Point is the offset of the character at text buffer.
+  } position;
+
+  Pixel() { Reset(); }
+
+  void Reset() {
+    shade.character = ' ';
+    memset(&position, -1, sizeof(position));
+  }
+
+  bool operator==(const Pixel &other) {
+    return memcmp(this, &other, sizeof(*this)) == 0;
+  }
+
+  Pixel &operator=(const Pixel &other) {
+    memcpy(this, &other, sizeof(*this));
+    return *this;
+  }
+
+  bool is_same_position(const Pixel &other) const {
+    return memcmp(&this->position, &other.position, sizeof(this->position)) ==
+           0;
+  }
+
+  bool is_same_shade(const Pixel &other) const {
+    return memcmp(&this->shade, &other.shade, sizeof(this->shade)) == 0;
+  }
+
+  bool is_head() const {
+    return position.head_tail_pair >> (sizeof(position.head_tail_pair) * 8 - 1);
+  }
+  size_t offset() const {
+    return position.head_tail_pair &
+           (static_cast<decltype(position.head_tail_pair)>(~0U) >> 1);
+  }
+  size_t length() const {
+    assert(is_head());
+    return offset();
+  }
+  static Pixel MakeHeadPixel(size_t point, size_t len, int c = 0) {
+    Pixel p;
+    assert(len < (1 << (sizeof(p.position.head_tail_pair) * 8 - 1)));
+    p.position.head_tail_pair =
+        (1 << (sizeof(p.position.head_tail_pair) * 8 - 1)) | len;
+    p.position.point = point;
+    p.shade.character = c;
+    return p;
+  }
+  static Pixel MakeTailPixel(size_t point, size_t offset, int c = 0) {
+    Pixel p;
+    assert(offset > 0 &&
+           offset < (1 << (sizeof(p.position.head_tail_pair) * 8 - 1)));
+    p.position.head_tail_pair = offset;
+    p.position.point = point;
+    p.shade.character = c;
+    return p;
+  }
+  static std::vector<Pixel> MakeSeries(size_t offset, size_t len, int c = 0) {
+    assert(len > 0);
+    std::vector<Pixel> ret;
+    ret.emplace_back(MakeHeadPixel(offset, len, c));
+    for (size_t i = 1; i < len; ++i)
+      ret.emplace_back(MakeTailPixel(offset, i, c));
+    return ret;
+  }
+};
+
+} // namespace emcc::tui
