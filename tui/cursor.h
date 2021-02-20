9e3822f0b5a243df374796962735bc08a594a870
Kai Luo <gluokai@gmail.com>
2021-02-20 13:38:23 +0800

    Add Region

--- tui/cursor.h
+++ tui/cursor.h
@@ -44,4 +44,25 @@ inline Cursor operator+(const Cursor lhs, const Cursor rhs) {
   return {lhs.y + rhs.y, lhs.x + rhs.x};
 }
 
+struct Region {
+  const size_t width;
+  Cursor begin, end;
+  Region(width, begin, end) : width(width), begin(begin), end(end) {
+    assert(begin.x < width && begin.x >= 0);
+    assert(end.x < width && end.x >= 0);
+    assert((end.y == begin.y && end.x >= begin.x) || end.y > begin.y);
+  }
+  size_t size() const { return Cursor::ComputeDistance(width, begin, end) + 1; }
+  bool contains(Cursor c) {
+    if (end.y == begin.y) {
+      return c.x >= begin.x && c.x <= end.x;
+    }
+    if (c.y == begin.y)
+      return c.x >= begin.x && c.x < width;
+    if (c.y == end.y)
+      return c.x >= 0 && c.x <= end.x;
+    return c.x >= 0 && c.x < width && c.y > begin.y && c.y < end.y;
+  }
+};
+
 } // namespace emcc::tui
