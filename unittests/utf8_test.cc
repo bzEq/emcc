ac5cadeab33a7f2fc9ad6634f3432d15df522cc2
Kai Luo <gluokai@gmail.com>
2021-03-06 17:11:40 +0800

    Add utf8 decoder

---
+++ unittests/utf8_test.cc
@@ -0,0 +1,33 @@
+#include "support/utf8.h"
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <stddef.h>
+#include <stdint.h>
+
+namespace {
+using namespace emcc;
+
+TEST(UTF8Test, Basic) {
+  std::string s("abcdefg");
+  size_t l = 0;
+  uint32_t state = 0, codepoint;
+  for (auto c : s) {
+    if (DecodeUTF8(&state, &codepoint, c) == UTF8_ACCEPT)
+      ++l;
+  }
+  EXPECT_TRUE(l == s.size());
+}
+
+TEST(UTF8Test, CN) {
+  std::string s("你好世界");
+  size_t l = 0;
+  uint32_t state = 0, codepoint;
+  for (auto c : s) {
+    if (DecodeUTF8(&state, &codepoint, c) == UTF8_ACCEPT)
+      ++l;
+  }
+  EXPECT_TRUE(l == 4);
+}
+
+} // namespace
